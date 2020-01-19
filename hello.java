98d47732e9ce6d4e73781f75ecbfe6d7fd586c3f
bensonyuen <byuen@my.hpu.edu>
2020-01-18 22:24:18 -1000

    added greeting method

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        greeting("hello");
+    }
+
+    public static void greeting(String msg){
+    	System.out.println(msg);
     }
 }
